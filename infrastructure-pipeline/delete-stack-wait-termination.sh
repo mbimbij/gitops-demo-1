77fa865554c14b41ceb2f4bdb3078b72dc476b9e
mbimbij <joseph.mbimbi@gmail.com>
2021-06-04 10:08:41 +0200

    - Deploy the stack (just an S3 bucket for the moment) - Reorganize the Makefile

--- infrastructure-pipeline/delete-stack-wait-termination.sh
+++ infrastructure-pipeline/delete-stack-wait-termination.sh
@@ -8,16 +8,15 @@ fi
 STACK_NAME=$1
 
 echo -e "##############################################################################"
-echo -e "force deleting S3 buckets for stack $STACK_NAME"
+echo -e "deleting stack $STACK_NAME"
 echo -e "##############################################################################"
+
 S3_BUCKETS=$(aws cloudformation describe-stack-resources --stack-name $STACK_NAME --query "StackResources[?ResourceType=='AWS::S3::Bucket'].PhysicalResourceId" --output text)
 for S3_BUCKET in $S3_BUCKETS
 do
+  echo -e "## force deleting S3 bucket $S3_BUCKET for stack $STACK_NAME"
   aws s3 rb s3://$S3_BUCKET --force
 done
 
-echo -e "##############################################################################"
-echo -e "deleting stack $STACK_NAME"
-echo -e "##############################################################################"
 aws cloudformation delete-stack --stack-name $STACK_NAME
 aws cloudformation wait stack-delete-complete --stack-name $STACK_NAME
